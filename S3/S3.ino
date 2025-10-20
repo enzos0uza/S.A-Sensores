aefd1ecddb20ada6882e0de348b324655ffc6243
pdrlbueno <pedro_h_bueno@estudante.sesisenai.org.br>
2025-10-20 12:20:44 -0300

    conexão com broken

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,21 +1,39 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
+const int PORT = 1883;
+const String URL = "test.mosquitto.org";
+const String Topic = "DSM2";
+const String broker_user = "";
+const String broker_pass = "";
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Conectando ao WiFi");
-  WiFi.begin(SSID,PASS);
+  WiFi.begin(SSID, PASS);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nConectado");
+  Serial.println("Conectando ao broker...");
+  mqtt.setServer(URL.c_str(), PORT);
+  while(!mqtt.connected()) {
+    String ID = "S1-";
+    ID += String(random(0xffff), HEX);
+    mqtt.connect(ID.c_str(), broker_user.c_str(), broker_pass.c_str());
     delay(200);
-  };
-  Serial.println("/nConectado!");
+    Serial.print(".");
+  }
+  Serial.println("\n Conectado ao broker com sucesso!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
