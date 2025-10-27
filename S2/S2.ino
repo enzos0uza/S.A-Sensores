df0f460ad03e0be667e9c6687b0bba8c947c896a
MassayoshiOgawa <psnnapoleandro@gmail.com>
2025-10-27 12:51:48 -0300

    feito a comunicação entre placa

--- S2/S2.ino
+++ S2/S2.ino
@@ -9,36 +9,62 @@ const String PASS = "8120gv08";
 
 const int PORT = 1883;
 const String URL = "test.mosquitto.org";
-const String Topic = "DSM2";
-const String broker_user = "";
-const String broker_pass = "";
+const String broker_user = ""; 
+const String broker_pass = ""; 
+const String MyTopic = "Topico_Leandro_123";
+const String OtherTopic = "Topico_Enzo_123";
+
+#define led 2
 
 void setup() {
+  pinMode(2, OUTPUT);
   Serial.begin(115200);
-  Serial.println("Conectando ao WiFi");
+  Serial.println("Conectando ao Wifi");
   WiFi.begin(SSID, PASS);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
-    delay(500);
+    delay(200);
   }
-  Serial.println("\nConectado");
-  Serial.println("Conectando ao broker...");
+  Serial.println("\nConectado!");
+  Serial.println("Conectando ao Broker...");
   mqtt.setServer(URL.c_str(), PORT);
-  while(!mqtt.connected()) {
+  while(!mqtt.connected()){
     String ID = "S1-";
     ID += String(random(0xffff), HEX);
     mqtt.connect(ID.c_str(), broker_user.c_str(), broker_pass.c_str());
     delay(200);
     Serial.print(".");
   }
+  mqtt.subscribe(MyTopic.c_str());
+  mqtt.setCallback(callback);
   Serial.println("\n Conectado ao broker com sucesso!");
 }
 
 void loop() {
-  String mensagem = "Leandro: ";
-  mensagem += "oi";
-  
-  mqtt.publish(Topic.c_str(), mensagem.c_str());
+  String mensagem = "";
+  if(Serial.available()>0){
+    mensagem += Serial.readStringUntil('\n');
+    mqtt.publish(OtherTopic.c_str(), mensagem.c_str());
+
+  }
+
   mqtt.loop();
   delay(2000);
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String mensagem = "";
+  for(int i = 0; i < length; i++){
+    mensagem += (char)payload[i];
+  }
+  Serial.print("Recebidos: ");
+  Serial.println(mensagem);
+  if (mensagem == "Enzo: acender") {
+    digitalWrite(2, HIGH);
+    Serial.print("AAAAH EU VO G: ");
+  } else {
+    digitalWrite(2, LOW);
+    Serial.print("errado  ");
+  }
 }
